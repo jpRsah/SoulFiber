7ebad98cd8a8c1b7f9f157524c9e8059e9be1145
salomir <salomir161@gmail.com>
2018-09-28 23:00:18 +0300

    new page-3

---
+++ src/components/contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Header from './header'
+function Contact() {
+        return(
+          <div>
+              <h1>Contact name</h1>
+              <Header/>
+          </div>
+        )
+}
+export default Contact
