92a2694208521a285db1872b15a191064a566f82
Kalinichenko Roman <Kalinichenko.Roman.webdev@gmail.com>
2019-01-11 22:32:17 +0300

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import razvivashki from '../images/index/развивашки.jpeg';
+import './style.scss'
 import Layout from '../components/layout'
 import { Card, Button, CardImg, CardTitle, CardText, CardColumns,
   CardBody } from 'reactstrap';
@@ -11,24 +12,25 @@ const IndexPage = (props) => {
 
   return (
     <Layout>
-            <Jumbotron>
-        <h1 className="display-3">Здравствуйте!</h1>
+      <Jumbotron>
+        <h1 className="hello">Здравствуйте!</h1>
         <p className="lead"> Меня зовут Роман. Я руководитель детского центра "Soul Fiber".</p>
         <hr className="my-2" />
-        <p>Этот сайт позволит Вам узнать о нашем центре более подробно.<br/> 
-        Вы сможете найти наши последние активности, 
-        познакомиться с преподавателями, 
-        узнать расписание и прочеть отзывы о нас.</p>
-        <p className="lead">
-          {/* <Button color="primary">Learn More</Button> */}
+        <p>
+          Этот сайт позволит Вам узнать о нашем центре более подробно.<br/> 
+          Вы сможете найти наши последние активности, 
+          познакомиться с преподавателями, 
+          узнать расписание и прочеть отзывы о нас.
         </p>
+        {/* <p className="lead">
+          <Button color="primary">Learn More</Button>
+        </p> */}
       </Jumbotron>
     <CardColumns>
       <Card>
         <CardImg top width="100%" src="https://pp.userapi.com/c638524/v638524847/36814/EdsuBR_XQMI.jpg" alt="Card image cap" />
         <CardBody>
           <CardTitle>Танцы</CardTitle>
-          
           <CardText>Современнные танцы по направлениям Break Dance и Hip Hop. Для детей с 4 лет.</CardText>
           <Link to="/break-dance" ><Button>подробнее</Button></Link>
         </CardBody>
@@ -47,31 +49,56 @@ const IndexPage = (props) => {
         <CardImg top width="100%" src="https://pp.userapi.com/c639919/v639919847/1e2d7/hu7kDDbVDEc.jpg" alt="Card image cap" />
         <CardBody>
           <CardTitle>Математика</CardTitle>
-          
-          <CardText></CardText>
-          <Link to="/math" ><Button>подробнее</Button></Link>
+          <CardText>Индивидуальные занятия.</CardText>
+          {/* <Link to="/math" ><Button>подробнее</Button></Link> */}
         </CardBody>
       </Card>
       
 
       <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
         <CardTitle>Логопедия</CardTitle>
-        <CardText>ехаль гека чевез веку,видет в веке вак,если все так,то обращайтесь)поможем,наверно.</CardText>
-        <Link to="/logopedia" ><Button>подробнее</Button></Link>
+        <CardText>Индивидуальные занятия.</CardText>
+        {/* <Link to="/logopedia" ><Button>подробнее</Button></Link> */}
       </Card>
       <Card>
         <CardImg top width="100%" src="https://pp.userapi.com/c849020/v849020470/47784/lFJhZGz3FkA.jpg" alt="Card image cap" />
         <CardBody>
-          <CardTitle>Основы программирования</CardTitle>
-          
-          <CardText>ну тут,тока не для тупых,давайте без вашего пафаса"мой ребенок очень умный и тд"а на практике.</CardText>
-          <Link to="/robot" ><Button>подробнее</Button></Link>
+          <CardTitle>Основы программирования и робототехники</CardTitle>
+          <CardText>Занятия в группах до 7 человек. 
+            {/* <ul>
+              <li>
+                Изучения основ языка программирования C++, разработка проектов на основе микроконтролера Arduino.
+              </li>
+              <li>
+                Основы Веб ремесла. 
+              </li>
+              <li>
+                Изучение устройство персонального компьютера. Пробная сборка. 
+              </li>
+              <li>
+                Знакомство с операционными системами на базе ядра. 
+              </li>
+            </ul> */}
+          </CardText>
+          {/* <Link to="/robot" ><Button>подробнее</Button></Link> */}
         </CardBody>
       </Card>
-      <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-        <CardTitle>А так же йога для уставших мам!</CardTitle>
-        <CardText>тока для красивых мам.</CardText>
-        <Link to="/mam" ><Button>подробнее</Button></Link>
+
+      <Card >
+        <CardImg top width="100%" src={razvivashki} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Развивашки</CardTitle>
+          <CardText>Сейчас много внимания уделяют развитию мелкой моторики￼, забывая про то, что детям просто необходимо бегать прыгать и шуметь для полноценного развития</CardText>
+          {/* <Link to="/mam" ><Button>подробнее</Button></Link> */}
+        </CardBody>
+      </Card>
+      <Card >
+        <CardImg top width="100%" src="http://www.hinfo.info/sites/default/files/styles/news-detail/public/field/image/stressmeditating0_0.jpg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Йога</CardTitle>
+          {/* <CardText></CardText> */}
+          {/* <Link to="/mam" ><Button>подробнее</Button></Link> */}
+        </CardBody>
       </Card>
       
     </CardColumns>
